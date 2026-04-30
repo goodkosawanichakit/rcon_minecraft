d3038deef792a930a909def614d90fd884422eec
goodkosawanichakit <goodkosawanichakit@gmail.com>
2026-04-30 17:40:33 +0700

    implemented Auth, even though it's have no fallback and security but that's future work

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
 	"fmt"
+	"io"
+	"math/rand"
 	"net"
 	"os"
 	"strings"
@@ -22,10 +25,9 @@ const (
 	LOGIN        = 3
 )
 
-func handleHelp() {
-	var help string = "I've no idea either, good luck finding it."
-	fmt.Println(help)
-	os.Exit(0)
+func makePacket(Type int32, payload string) Packet {
+	var rawPayload []byte = append([]byte(payload), '\x00', '\x00')
+	return Packet{Length: 8 + int32(len(rawPayload)), Id: rand.Int31(), Type: Type, Payload: rawPayload}
 }
 
 func makeBuffer(packet *Packet) []byte {
@@ -37,22 +39,8 @@ func makeBuffer(packet *Packet) []byte {
 	return buf.Bytes()
 }
 
-func handleAuthentication(conn net.Conn) error {
-	var pass []byte = append([]byte(os.Getenv("RCON_PASSWD")), '\x00', '\x00')
-	var length int32 = 8 + int32(len(pass))
-
-	var packet Packet = Packet{Length: length, Id: 7, Type: LOGIN, Payload: pass}
-
-	var rawBytes []byte = makeBuffer(&packet)
+func sendPacket(conn net.Conn, rawBytes []byte) error {
 	_, err := conn.Write(rawBytes)
-
-	if err != nil {
-		return err
-	}
-
-	var response [4096]byte
-	n, err := conn.Read(response[:])
-
 	if err != nil {
 		return err
 	}
@@ -60,18 +48,51 @@ func handleAuthentication(conn net.Conn) error {
 	return nil
 }
 
-func handleCommand(conn net.Conn) error {
-	var cmd []byte = append([]byte(strings.Join(os.Args[1:len(os.Args)], " ")), '\x00', '\x00')
-	var length int32 = 8 + int32(len(cmd))
+func readPacket(conn net.Conn) (Packet, error) {
+	var length int32
 
-	var packet Packet = Packet{Length: length, Id: 3, Type: COMMAND, Payload: cmd}
+	err := binary.Read(conn, binary.LittleEndian, &length)
+	if err != nil {
+		return Packet{}, err
+	}
+
+	var buf []byte = make([]byte, length)
+
+	_, err = io.ReadFull(conn, buf)
+	if err != nil {
+		return Packet{}, err
+	}
+
+	id := int32(binary.LittleEndian.Uint32(buf[0:4]))
+	Type := int32(binary.LittleEndian.Uint32(buf[4:8]))
+	payload := buf[8:]
+
+	return Packet{Length: length, Id: id, Type: Type, Payload: payload}, err
+}
+
+func handleHelp() {
+	var help string = "I've no idea either, good luck finding it."
+	fmt.Println(help)
+	os.Exit(0)
+}
+
+func handleAuthentication(conn net.Conn) error {
+	var packet Packet = makePacket(LOGIN, os.Getenv("RCON_PASSWD"))
 
 	var rawBytes []byte = makeBuffer(&packet)
 
-	_, err := conn.Write(rawBytes)
-
+	err := sendPacket(conn, rawBytes)
 	if err != nil {
 		return err
+	}
+
+	packet, err = readPacket(conn)
+	if err != nil {
+		return err
+	}
+
+	if packet.Id == -1 {
+		return errors.New("wrong password")
 	}
 
 	return nil
@@ -100,13 +121,7 @@ func main() {
 	defer conn.Close()
 
 	if error := handleAuthentication(conn); error != nil {
-		fmt.Println("Auth falied:", error)
+		fmt.Println("Authentication falied:", error)
 		os.Exit(1)
 	}
-
-	if error := handleCommand(conn); error != nil {
-		fmt.Println("What?")
-		os.Exit(1)
-	}
-
 }
